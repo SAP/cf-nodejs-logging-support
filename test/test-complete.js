39f121c23e3d00caf5150a7f073bd740e853cc56
Nicklas Dohrn <nicklas.dohrn@sap.com>
2017-07-14 14:00:43 +0200

    Revert "completed new test for testing whole logging pipeline"
    This reverts commit 8bba2a29ac696a301c7f40f1a74c8661e9cddf78.

--- test/test-complete.js
+++ test/test-complete.js
@@ -1,67 +1,66 @@
 var chai = require("chai");
 var sinon = require("sinon");
-var httpMock = require("node-mocks-http");
 var assert = chai.assert;
 chai.should();
 
-Function.prototype.override = function (func) {
-    var superFunction = this;
-    return function () {
-        this.superFunction = superFunction;
-        return func.apply(this, arguments);
-    };
-};
-
 describe('Test Complete', function () {
-    var store;
 
-    var log = require("../index");
-    var results = require("./exp-results");
-
-    var prepare = function (res) {
-        res.names = [];
-        self = res;
-        res.on = function (name, func) {
-            self.names[name] = func;
-        }
-        res.emit = function (name) {
-            if (self.names[name]) {
-                self.names[name]();
-            }
-        }
-    }
-
+    var dummyStdout = {
+        "written_at": "2017-07-07T09:40:15.961Z",
+        "written_ts": 0,
+        "component_type": "application",
+        "component_id": "-",
+        "component_name": "-",
+        "component_instance": "0",
+        "layer": "[NODEJS]",
+        "space_name": "-",
+        "space_id": "-",
+        "source_instance": "0",
+        "container_id": "-",
+        "logger": "nodejs-logger",
+        "msg": "listening on port: 3000",
+        "type": "log",
+        "level": "info"
+    };
+    var store = [];
+    var storeCount = 0;
     var origStdout = process.stdout.write;
-    process.stdout.write = process.stdout.write.override(
-        function (obj) {
-            store = obj;
-            this.superFunction(obj);
-        }
-    );
     before(function () {
+        var log = require("../index");
+        var http = require('http');
+        var request = require('request');
+        process.stdout.write = (obj) => {
+            store[storeCount++] = obj;
+        }
 
-            process.hrtime = function () {
-                return [12, 14];
-            }
-        clock = sinon.useFakeTimers();
+        //forces logger to run the http version.
+        log.forceLogger("plainhttp");
+
+        var server = http.createServer((req, res) => {
+            //binds logging to the given request for request tracking
+            log.logNetwork(req, res);
+
+            // Context bound custom message
+            req.logMessage("info", "request bound information:", {
+                "some": "info"
+            });
+            res.end('ok');
+        });
+        server.listen(3000);
+        // Formatted log message free of request context
+        log.logMessage("info", "Server is listening on port %d", 3000);
+        request.get("localhost:3000").on("response", callback);
     });
 
-    after(function () {
-        clock.restore();
+    beforeEach(() => {
         process.stdout.write = origStdout;
     });
 
     it("checking dummy app results", () => {
-        req = httpMock.createRequest( {
-            headers: {
-                "X-CorrelationID": "uuid-Dummy"
-            }
-        });
-        res = httpMock.createResponse();
-        prepare(res);
-
-        log.logNetwork(req, res, () => {});
-        res.end("ok");
-       results.getLogMessage().should.equal(store);
+        console.log(store);
     });
 });
+
+var callback = function (res) {
+    console.log(res);
+}
