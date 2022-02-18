0c1cbdd543aed500403d78757e29be39040ca564
Federico Romagnoli <federico.romagnoli@sap.com>
2022-02-18 13:59:55 +0100

    remove code duplication to add event listener

--- sample/cf-nodejs-logging-support-sample/public/scripts/logic.js
+++ sample/cf-nodejs-logging-support-sample/public/scripts/logic.js
@@ -1,46 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const globalContext = document.getElementById("global-context");
-    const requestContext = document.getElementById("request-context");
-    const customFields = document.getElementById("custom-fields");
-    const stackTrace = document.getElementById("stack-trace");
-    const clientError = document.getElementById("client-error");
-    const childLogger = document.getElementById("child-logger");
-    const correlationTenantId = document.getElementById("correlation-and-tenant-id");
+    const collection = Array.from(document.getElementsByClassName("btn"));
 
-    globalContext.addEventListener("click", () => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "globalcontext");
-        xhttp.send();
-    });
-    requestContext.addEventListener("click", () => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "requestcontext");
-        xhttp.send();
-    });
-    customFields.addEventListener("click", () => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "customfields");
-        xhttp.send();
-    });
-    stackTrace.addEventListener("click", () => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "stacktrace");
-        xhttp.send();
-    });
-    clientError.addEventListener("click", () => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "unknown");
-        xhttp.send();
-    });
-    childLogger.addEventListener("click", () => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "childlogger");
-        xhttp.send();
-    });
-    correlationTenantId.addEventListener("click", () => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "correlationandtenantid");
-        xhttp.send();
-    });
+    collection.forEach(
+        (element) => {
+            element.addEventListener("click", () => {
+                var xhttp = new XMLHttpRequest();
+                xhttp.open("GET", element.getAttribute('targetpath'));
+                xhttp.send();
+            });
+        }
+    );
 })
