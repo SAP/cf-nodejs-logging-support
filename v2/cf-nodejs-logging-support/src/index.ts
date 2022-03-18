ee1a1e0ee357b1387c56d749589b053540d4f5cd
Federico Romagnoli <federico.romagnoli@sap.com>
2022-03-18 10:08:30 +0100

    set config in index.ts

--- v2/cf-nodejs-logging-support/src/index.ts
+++ v2/cf-nodejs-logging-support/src/index.ts
@@ -1,1 +1,12 @@
 export * from './lib/core/extern-api-methods';
+import { ConfigFile } from './lib/interfaces';
+import * as coreConfig from './lib/config/config-core.json';
+import * as requestConfig from './lib/config/config-request.json';
+import * as customConfig from './lib/config/config-custom-test.json';
+import { Config } from './lib/config/config';
+let config = new Config(
+    coreConfig as ConfigFile, 
+    requestConfig as ConfigFile, 
+    customConfig as ConfigFile
+    );
+console.info(config.getConfig());
