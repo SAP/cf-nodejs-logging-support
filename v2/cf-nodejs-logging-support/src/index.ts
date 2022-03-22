8615c2da49e4591b38bd7fa5382b83113d3625ee
Federico Romagnoli <federico.romagnoli@sap.com>
2022-03-22 11:04:43 +0100

    update index.ts

--- v2/cf-nodejs-logging-support/src/index.ts
+++ v2/cf-nodejs-logging-support/src/index.ts
@@ -2,27 +2,54 @@ export * from './lib/core/extern-api-methods';
 import { ConfigFile } from './lib/interfaces';
 import * as coreConfig from './lib/config/config-core.json';
 import * as requestConfig from './lib/config/config-request.json';
+import * as cfConfig from './lib/config/config-cf.json';
+import * as kymaConfig from './lib/config/config-kyma.json';
+import * as appLoggingConfig from './lib/config/config-app-logging.json';
+import * as cloudLoggingConfig from './lib/config/config-cloud-logging.json';
 import { Config } from './lib/config/config';
+import * as EnvManagement from './lib/core/env-management';
 
 let config = new Config(
     coreConfig as ConfigFile,
     requestConfig as ConfigFile
 );
+let env = EnvManagement.getEnv();
+let boundServices = EnvManagement.getBoundServices();
+
+if (env == "CF") {
+    config.addConfig([cfConfig as ConfigFile]);
+}
+
+if (env == "Kyma") {
+    config.addConfig([kymaConfig as ConfigFile]);;
+}
+
+if (boundServices["application-logging"]) {
+    config.addConfig([appLoggingConfig as ConfigFile]);
+}
+
+if (boundServices["cloud-logging"]) {
+    config.addConfig([cloudLoggingConfig as ConfigFile]);
+}
 
 export function addConfig(...configs: ConfigFile[]) {
     return config.addConfig(configs);
 }
 
-export function getConfig(...fieldNames: string[]) {
-    return config.getConfig(fieldNames);
+export function getConfig() {
+    return config.getConfig();
 }
 
-export function getCoreConfig() {
-    return config.getCoreConfig();
+export function getFields(...fieldNames: string[]) {
+    return config.getFields(fieldNames);
 }
 
-export function getReqConfig() {
-    return config.getReqConfig();
+export function getCoreFields() {
+    return config.getCoreFields();
+}
+
+export function getReqFields() {
+    return config.getReqFields();
 }
 
 export function getDeactivatedFields() {
