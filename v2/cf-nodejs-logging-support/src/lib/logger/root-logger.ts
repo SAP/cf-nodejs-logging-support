883550da9373ad88e1069de2f57515002e5a8544
Federico Romagnoli <federico.romagnoli@sap.com>
2022-03-28 14:43:41 +0200

    add config methods to root-logger

--- v2/cf-nodejs-logging-support/src/lib/logger/root-logger.ts
+++ v2/cf-nodejs-logging-support/src/lib/logger/root-logger.ts
@@ -1,15 +1,47 @@
 import Level from "./level"
 import Logger from "./logger"
+import Config from "../config/config"
+import { ConfigObject, customFieldsFormat } from "../config/interfaces"
+import EnvManagement from "../core/env-management";
 
 export default class RootLogger extends Logger {
+    private config = Config.getInstance();
 
     constructor() {
         super()
         this.loggingLevelThreshold = Level.INFO
     }
 
-    addConfig(_object: Object) {
-        // todo: add to config
+    getConfig() {
+        return this.config.getConfig();
+    }
+
+    getFields(...fieldNames: string[]) {
+        return this.config.getFields(fieldNames);
+    }
+
+    getMsgFields() {
+        return this.config.getMsgFields();
+    }
+
+    getReqFields() {
+        return this.config.getReqFields();
+    }
+
+    getDeactivatedFields() {
+        return this.config.getDeactivatedFields();
+    }
+
+    addConfig(...configObject: ConfigObject[]) {
+        return this.config.addConfig(configObject);
+    }
+
+    setCustomFieldsFormat(format: customFieldsFormat) {
+        return this.config.setCustomFieldsFormat(format);
+    }
+
+    setStartupMessageEnabled(enabled: boolean) {
+        return this.config.setStartupMessageEnabled(enabled);
     }
 
     setSinkFunction(_f: Function) { }
@@ -20,7 +52,9 @@ export default class RootLogger extends Logger {
 
     registerCustomFields(_object: Object) { }
 
-    getBoundServices() { }
+    getBoundServices() { 
+        return EnvManagement.getBoundServices()
+    }
 
     // legacy methods
     overrideNetworkField(_field: string, _value: string) { }
