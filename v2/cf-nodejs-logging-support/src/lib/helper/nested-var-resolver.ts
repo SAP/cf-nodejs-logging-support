a41ee2defba49ad6ab6d4ec2680a0be6bddcfab4
Federico Romagnoli <federico.romagnoli@sap.com>
2022-05-03 10:14:02 +0200

    create helper class NestedVarResolver

---
+++ v2/cf-nodejs-logging-support/src/lib/helper/nested-var-resolver.ts
@@ -0,0 +1,34 @@
+export default class NestedVarResolver {
+    static resolveNestedVariable(root: any, path: string[]): any {
+
+        // return, if path is empty.
+        if (path == null || path.length == 0) {
+            return null;
+        }
+
+        var rootObj;
+
+        // if root is a string => parse it to an object. Otherwise => use it directly as object.
+        if (typeof root === "string") {
+            rootObj = JSON.parse(root);
+        } else if (typeof root === "object") {
+            rootObj = root;
+        } else {
+            return null;
+        }
+
+        // get value from root object
+        var value = rootObj[path[0]];
+
+        // cut first entry of the object path
+        path.shift();
+
+        // if the path is not empty, recursively resolve the remaining waypoints.
+        if (path.length >= 1) {
+            return NestedVarResolver.resolveNestedVariable(value, path);
+        }
+
+        // return the resolved value, if path is empty.
+        return value;
+    };
+}
