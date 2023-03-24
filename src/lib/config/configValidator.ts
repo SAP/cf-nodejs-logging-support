7bc1aaa931eb775e00e2ddbc953f46c6ab5e5954
Christian Dinse <christian.dinse@sap.com>
2023-03-24 13:04:14 +0100

    Switch to singleton pattern

--- src/lib/config/configValidator.ts
+++ src/lib/config/configValidator.ts
@@ -1,15 +1,20 @@
-import ConfigShema from "./default/config-schema.json";
-import Ajv from "ajv";
+import Ajv, { ValidateFunction } from 'ajv';
+
+import ConfigSchema from './default/config-schema.json';
 
 export default class ConfigValidator {
 
-    private static ajv = new Ajv({ strict: false });
+    private validate: ValidateFunction
 
-    static isValid(config: any): true | [false, any] {
-        const validate = ConfigValidator.ajv.compile(ConfigShema);
-        const valid = validate(config);
+    constructor() {
+        const ajv = new Ajv({ strict: false });
+        this.validate = ajv.compile(ConfigSchema);
+    }
+
+    isValid(config: any): true | [false, any] {
+        const valid = this.validate(config);
         if (!valid) {
-            return [false, validate.errors];
+            return [false, this.validate.errors];
         }
         return true;
     }
