2b2c964f77d7bcceaa6a0ba138c882fbccafaf1a
Christian Dinse <christian.dinse@sap.com>
2023-04-24 17:17:28 +0200

    Add module exports for CommonJS and ES6 imports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ import RootLogger from "./lib/logger/rootLogger"
 
 const rootLogger = RootLogger.getInstance();
 
-export = rootLogger
+module.exports = rootLogger; // assign default export
+exports = module.exports; // re-assign exports
+
+export default rootLogger
+export * from "./lib/config/interfaces"
+export * from "./lib/logger/level";
