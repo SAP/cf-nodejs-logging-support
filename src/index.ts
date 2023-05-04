69e9d438494bbdd21d3890b961f79c6e46ad8f37
Christian Dinse <christian.dinse@sap.com>
2023-05-04 09:13:11 +0200

    Fix missing newline

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import RootLogger from "./lib/logger/rootLogger"
+import RootLogger from "./lib/logger/rootLogger";
 
 const rootLogger = RootLogger.getInstance();
 
 module.exports = rootLogger; // assign default export
 exports = module.exports; // re-assign exports
 
-export default rootLogger
-export * from "./lib/config/interfaces"
+export default rootLogger;
+export * from "./lib/config/interfaces";
 export * from "./lib/logger/level";
