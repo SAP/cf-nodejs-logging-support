9d6e3a82eb73f1c80c1d3356cc3df0699ba3c238
nicklas dohrn <nicklas.dohrn@sap.com>
2023-09-29 13:55:05 +0200

    Update sample app with new features
    This changes and adds two main features:
    - The last sent log message is now shown in the Client UI
    - The currently active service bindings are listed in the UI
    
    Co-authored-by: Christian Dinse <19688125+christiand93@users.noreply.github.com>

--- sample/cf-nodejs-logging-support-sample/app.js
+++ sample/cf-nodejs-logging-support-sample/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-var log = require("cf-nodejs-logging-support");
+const log = require("cf-nodejs-logging-support");
 const app = express();
+var lastMessage = null;
 
 // roots the views directory to public
 app.set('views', 'public');
@@ -12,6 +13,11 @@ app.use(express.static(__dirname + "/public"));
 
 // add logger to the server network queue to log all incoming requests.
 app.use(log.logNetwork);
+
+log.setSinkFunction((_, msg) => {
+  lastMessage = msg
+  console.log(msg)
+});
 
 // set the logging level threshold
 log.setLoggingLevel("info");
@@ -39,7 +45,7 @@ app.get("/", function (req, res) {
 // https://sap.github.io/cf-nodejs-logging-support/general-usage/logging-contexts#global-context
 app.get("/globalcontext", function (req, res) {
   log.info("Message logged in global context");
-  res.send();
+  res.send(lastMessage);
 });
 
 // demonstrate log in request context
@@ -47,14 +53,21 @@ app.get("/globalcontext", function (req, res) {
 app.get("/requestcontext", function (req, res) {
   var reqLogger = req.logger; // reqLogger logs in request context
   reqLogger.info("Message logged in request context");
-  res.send();
+  res.send(lastMessage);
 });
 
 // log message with some custom fields in global context 
 // https://sap.github.io/cf-nodejs-logging-support/general-usage/custom-fields
 app.get("/customfields", function (req, res) {
   log.info("Message logged in global context with some custom fields", { "custom-field-a": "value-a", "custom-field-b": "value-b" });
-  res.send();
+  res.send(lastMessage);
+});
+
+// log message with some custom fields in global context 
+// https://sap.github.io/cf-nodejs-logging-support/general-usage/custom-fields
+app.get("/passport", function (req, res) {
+  log.info("Message with passport", { "sap_passport": "2A54482A0300E60000756E64657465726D696E6564202020202020202020202020202020202020202000005341505F4532455F54415F557365722020202020202020202020202020202020756E64657465726D696E65645F737461727475705F302020202020202020202020202020202020200005756E64657465726D696E6564202020202020202020202020202020202020202034323946383939424439414334374342393330314345463933443144453039432020200007793BCF7D8152423B8A7FD073109C45CE0000000000000000000000000000000000000000000000E22A54482A" });
+  res.send(lastMessage);
 });
 
 // demonstrate an error stack trace logging
@@ -65,7 +78,7 @@ app.get("/stacktrace", function (req, res) {
     res.send("request succesful");
   } catch (e) {
     log.error("Error occurred", e)
-    res.status(500).send("error ocurred");
+    res.status(500).send(lastMessage);
   }
 });
 
@@ -79,7 +92,7 @@ app.get("/childlogger", function (req, res) {
   var subLogger = log.createLogger({ "new-field": "value" });
   subLogger.setLoggingLevel("warn");
   subLogger.warn("Message logged from child logger.");
-  res.send();
+  res.send(lastMessage);
 });
 
 // get the correlation and tenant ID by calling req.logger.getCorrelationId() and .getTenantId()
@@ -89,8 +102,22 @@ app.get("/correlationandtenantid", function (req, res) {
   var correlationId = reqLogger.getCorrelationId();
   var tenantId = reqLogger.getTenantId();
   reqLogger.info("Correlation ID: %s Tenant ID: %s", correlationId, tenantId);
-  res.send();
+  res.send(lastMessage);
 });
+
+app.get("/binding_information", function (_, res) {
+  result = "There are the following bindings: <br>"
+  var services
+  if (process.env.VCAP_SERVICES)
+    services = JSON.parse(process.env.VCAP_SERVICES)
+  if (services == undefined)
+    res.send("There are no bindings present for this application")
+  else {
+    for (var service in services)
+      result += service + "<br>"
+    res.send(result)
+  }
+})
 
 
 // binds the express module to 'app', set port and run server
