3d0f1dfe2dad8a0edc2f5afb7f41c4b70aa165a0
Christian Dinse <christian.dinse@sap.com>
2024-02-19 17:33:27 +0100

    Add tests for fastify

--- src/test/acceptance-test/request-context.test.js
+++ src/test/acceptance-test/request-context.test.js
@@ -7,13 +7,13 @@ const supertest = require('supertest');
 const expressApp = require("./express/app.js");
 const restifyApp = require("./restify/app.js");
 const connectApp = require("./connect/app.js");
+const fastifyApp = require("./fastify/app.js");
 const httpApp = require("./nodejs-http/app.js");
 const log = importFresh("../../../build/main/index");
 
-var lastLogs;
+var lastLogs = [];
 
 describe('Test request context', function () {
-
     // catch logs written by supertest app
     process.writeLog = function (level, output) {
         lastLogs.push(JSON.parse(output));
@@ -25,7 +25,7 @@ describe('Test request context', function () {
         });
     });
 
-    describe("Express", function () {
+    describe("Use Express framework", function () {
         before(function () {
             log.forceLogger("express");
         });
@@ -276,7 +276,7 @@ describe('Test request context', function () {
         });
     })
 
-    describe("RESTIFY", function () {
+    describe("Use Restify framework", function () {
 
         before(function (done) {
             log.forceLogger("restify");
@@ -359,7 +359,7 @@ describe('Test request context', function () {
         });
     });
 
-    describe("CONNECT", function () {
+    describe("Use Connect framework", function () {
 
         before(function (done) {
             log.forceLogger("connect");
@@ -444,7 +444,95 @@ describe('Test request context', function () {
         });
     });
 
-    describe("NODEJSHTTP", function () {
+    describe("Use Fastify framework", function () {
+        before(function (done) {
+            log.forceLogger("fastify");
+            done();
+        });
+
+        describe("Set implicit correlation- and tenant-id through request header", function () {
+            var correlation_id = "cbc4343f-1c31-27d0-96fc-f32efac20986";
+            var tenant_id = "abc2654f-5t15-12h0-78gt-n73jeuc01847";
+
+            before(function (done) {
+                fastifyApp.ready().then(() => {
+                    supertest(fastifyApp.server)
+                        .get("/log")
+                        .set("x-correlationid", correlation_id)
+                        .set("tenantid", tenant_id)
+                        .expect(200)
+                        .then(done())
+                        .catch(err => done(err));
+                })
+            });
+
+            it("sets correlation_id via header", function () {
+                expect(lastLogs.length).to.be.gt(1);
+                expect(lastLogs[1]).to.have.property('correlation_id', correlation_id);
+            });
+
+            it("sets tenant_id via header", function () {
+                expect(lastLogs[1]).to.have.property('tenant_id', tenant_id);
+            });
+
+            after(function () {
+                lastLogs = [];
+            });
+        });
+
+        describe("Set implicit correlation- and tenant-id through methods", function () {
+
+            var correlation_id = "cbc2654f-1c35-45d0-96fc-f32efac20986";
+            var tenant_id = "abc8714f-5t15-12h0-78gt-n73jeuc01847";
+
+            before(function (done) {
+                fastifyApp.ready().then(() => {
+                    supertest(fastifyApp.server)
+                        .get("/setcorrelationandtenantid")
+                        .expect(200)
+                        .then(done())
+                        .catch(err => done(err));
+                })
+            });
+
+            it("writes a log with correlation id", function () {
+                expect(lastLogs.length).to.be.gt(1);
+                expect(lastLogs[1]).to.have.property('correlation_id', correlation_id);
+            });
+
+            it("sets tenant_id via header", function () {
+                expect(lastLogs[1]).to.have.property('tenant_id', tenant_id);
+            });
+
+            after(function () {
+                lastLogs = [];
+            });
+        });
+
+        describe("Get correlation- and tenant-id", function () {
+            before(function (done) {
+                fastifyApp.ready().then(() => {
+                    supertest(fastifyApp.server)
+                        .get("/getcorrelationandtenantid")
+                        .expect(200)
+                        .then(done())
+                        .catch(err => done(err));
+                })
+            });
+
+            it("get methods returned expected values", function () {
+                expect(lastLogs.length).to.be.gt(0);
+                expect(lastLogs[0]).to.have.property('msg', 'successful');
+            });
+
+
+            after(function () {
+                lastLogs = [];
+            });
+        });
+    });
+
+    describe("Use Node.js http", function () {
         before(function (done) {
             log.forceLogger("plainhttp");
             done();
