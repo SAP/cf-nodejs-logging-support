391a79f53f91399b097a240a4393e70c3bfb3686
Christian Dinse <christian.dinse@sap.com>
2024-03-11 13:39:32 +0100

    Implement OpenTelemetry plugin

--- src/lib/logger/record.ts
+++ src/lib/logger/record.ts
@@ -1,11 +1,34 @@
-export default class Record {
-    payload: any
-    metadata: any
+import { Level } from "./level"
 
-    constructor(level: string) {
+export class Record {
+    payload: { [key: string]: any }
+    metadata: RecordMetadata
+
+    constructor(type: RecordType, level: Level) {
         this.payload = {}
-        this.metadata = {
-            level: level
-        }
+        this.metadata = new RecordMetadata(type, level)
     }
+}
+
+export class RecordMetadata {
+    type: RecordType
+    level: Level
+    message?: string
+    rawStacktrace?: string
+    stacktrace?: string[]
+    errorName?: string
+    errorMessage?: string
+    customFieldNames: string[]
+
+    constructor(type: RecordType, level: Level) {
+        this.type = type
+        this.level = level
+        this.customFieldNames = new Array<string>()
+    }
+}
+
+export type RecordFieldValue = string | string[] | number | boolean
+
+export enum RecordType {
+    Request, Message
 }
